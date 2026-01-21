a69bdf8ea89ece069de4a49218664a722d644f69
João Vitor Soares <joaovitorsoares12@gmail.com>
2026-01-21 04:18:10 -0300

    Refactor Prisma schema and migrations
    - Cleaned up the `schema.prisma` file for better readability by adjusting formatting.
    - Removed outdated migration files related to the `log_daily_summaries` table.
    - Added a new migration file to initialize the database with `users`, `logs`, and `log_daily` tables, along with associated enums for user roles and log levels.
    
    These changes streamline the database structure and improve the clarity of the Prisma schema.

--- prisma/migrations/20260121071611_init/migration.sql
+++ prisma/migrations/20260121071611_init/migration.sql
@@ -40,6 +40,20 @@ CREATE TABLE "logs" (
     CONSTRAINT "logs_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "log_daily" (
+    "id" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "info" INTEGER NOT NULL DEFAULT 0,
+    "warning" INTEGER NOT NULL DEFAULT 0,
+    "error" INTEGER NOT NULL DEFAULT 0,
+    "debug" INTEGER NOT NULL DEFAULT 0,
+    "critical" INTEGER NOT NULL DEFAULT 0,
+    "total" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "log_daily_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
@@ -54,3 +68,6 @@ CREATE INDEX "logs_deviceId_idx" ON "logs"("deviceId");
 
 -- CreateIndex
 CREATE INDEX "logs_userId_idx" ON "logs"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "log_daily_date_key" ON "log_daily"("date");
